52f65c8702acf300d3c9d0317b1a79eac6ec35f4
Ashika Vijay <ashika.vijay@malinkoapp.com>
2019-11-26 15:13:58 +0000

    finished piggy.go exercise

---
+++ unit_2/piggy.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	piggyBank := 0.0
+	nickels := 0.05
+	dimes := 0.10
+	quarters := 0.25
+
+	for piggyBank < 20.00 {
+		switch rand.Intn(3) {
+		case 0:
+			piggyBank += nickels
+		case 1:
+			piggyBank += dimes
+		case 2:
+			piggyBank += quarters
+		}
+
+		fmt.Printf("$%5.2f\n", piggyBank)
+	}
+
+}
