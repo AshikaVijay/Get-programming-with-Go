b2ee1603234e935a4fb54b7c9c1dfda4abe69d3a
Ashika Vijay <ashika.vijay@malinkoapp.com>
2019-11-26 15:46:55 +0000

    second piggybank exercises with dollars and cents

---
+++ unit_2/piggy_2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	piggyBank := 0
+	nickels := 5
+	dimes := 10
+	quarters := 25 //cents
+
+	for piggyBank < 2000 {
+		switch rand.Intn(3) {
+		case 0:
+			piggyBank += nickels
+		case 1:
+			piggyBank += dimes
+		case 2:
+			piggyBank += quarters
+		}
+
+		piggyBankDollars := piggyBank / 100
+		piggyBankCents := piggyBank % 100
+
+		fmt.Printf("$%d.%02d\n", piggyBankDollars, piggyBankCents)
+	}
+}
